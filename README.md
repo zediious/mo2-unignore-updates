c2c3d0c23269007f09c0ce7eb56248cdad9ca317
zediious <zediious@aol.com>
2023-08-28 21:03:02 -0400

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mod Organizer 2 - Un-ignore downloads
+
+This script will make all mods in a Mod Organizer list no longer ignore updates, after they have been told to do so via the context menu.
+
+If you have done this to a lot of mods, this can help you quickly fix that up.
+
+## Instructions to run
+
+1) Download Python https://www.python.org/downloads/
+
+2) Download the [LATEST RELEASE](https://github.com/zediious/mo2-unignore-updates/releases) and place the `.py` file inside your Mod Organizer 2 `mods` directory.
+
+3) Open your terminal of choice, and move to your `mods` directory. Run `python unignore.py` once you are in the `mods` directory.
+
+This will replace any instances of `ignoredVersion=` that are followed by any non-whitespace characters within a `meta.ini` file for any mod, with only `ignoredVersion=`. It will only affect files that are not already un-ignored. A log will be printed to the terminal of all affected mods, and that log will be saved in the `mods` directory.
---
+++ mo2-unignore-updates/unignore.py
@@ -0,0 +1,52 @@
+from os import chdir, listdir
+from re import compile, sub
+
+ignored_regex = compile('ignoredVersion=\S+')
+
+# Open the log file to write changes to as the script runs
+with open('mo2-unignore.log', 'w') as log_file:
+
+    log_file.write("List of mods that will no longer ignore updates;\n")
+
+    # Iterate through mod directories
+    for mod_dir in listdir():
+        
+        # Check for separators and files related to this program, and skip them
+        if (str(mod_dir).__contains__('_separator')
+        or str(mod_dir).__contains__('mo2-unignore.log')
+        or str(mod_dir).__contains__('unignore.py')):
+            continue
+        
+        # Move into mod directory
+        chdir(str(mod_dir))
+        
+        # Read the mod's meta.ini file to memory, and remove ignored version data
+        file_data: str
+        changed_file_data: str
+        try:
+            with open('meta.ini', "r") as read_file:
+                file_data = read_file.read()
+                changed_file_data = sub(ignored_regex, 'ignoredVersion=', file_data)
+        except FileNotFoundError:
+            print(f'The mod "{mod_dir}" did not have a meta.ini file')
+        
+        # Write changed data from memory to meta.ini file if the data changed
+        if file_data != changed_file_data:
+            try:
+                with open('meta.ini', 'w') as write_file:
+                    write_file.write(changed_file_data)
+                    # Log the changed mod
+                    log_file.write(f"{str(mod_dir)}\n")
+            except FileNotFoundError:
+                print(f'The mod "{mod_dir}" did not have a meta.ini file')
+        
+        # Move back up to main mods directory    
+        chdir('..')
+
+# Print the log
+with open('mo2-unignore.log', 'r') as log_file:
+
+    print(" ")
+    for line in log_file:
+
+        print(line.strip())
